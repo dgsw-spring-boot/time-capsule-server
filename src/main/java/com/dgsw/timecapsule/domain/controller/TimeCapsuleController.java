c87a7b114de749b478ac68737e0f805b185de787
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-06-22 12:14:11 +0900

    capsule 생성

--- src/main/java/com/dgsw/timecapsule/domain/controller/TimeCapsuleController.java
+++ src/main/java/com/dgsw/timecapsule/domain/controller/TimeCapsuleController.java
@@ -1,11 +1,24 @@
 package com.dgsw.timecapsule.domain.controller;
 
+import com.dgsw.timecapsule.domain.dto.CapsuleRequestDTO;
+import com.dgsw.timecapsule.domain.dto.CapsuleResponseDTO;
+import com.dgsw.timecapsule.domain.service.CapsuleService;
+import com.dgsw.timecapsule.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/timecapsule")
 @RequiredArgsConstructor
 public class TimeCapsuleController {
+    private final CapsuleService capsuleService;
+
+    @PostMapping("/capsule")
+    public ApiResponse<CapsuleResponseDTO> createCapsule(@RequestBody CapsuleRequestDTO request) {
+        return ApiResponse.ok(capsuleService.createCapsule(request));
+    }
 }
---
+++ src/main/java/com/dgsw/timecapsule/domain/dto/CapsuleResponseDTO.java
@@ -0,0 +1,10 @@
+package com.dgsw.timecapsule.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CapsuleResponseDTO {
+    private String message;
+}
