3a4791f6909e9a79e7f83d7c8797909f8405a323
황정빈 <hjbin1211@gmail.com>
2026-06-22 11:37:05 +0900

    feat: controller, repository 기본 구성

---
+++ src/main/java/com/dgsw/timecapsule/domain/repository/TimeCapsuleRepository.java
@@ -0,0 +1,9 @@
+package com.dgsw.timecapsule.domain.repository;
+
+import com.dgsw.timecapsule.domain.entity.TimeCapsule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TimeCapsuleRepository extends JpaRepository<TimeCapsule, Long> {
+}
