68abd5b3c9f68bdb0e8510d5fcc959798e49575d
황정빈 <hjbin1211@gmail.com>
2026-06-22 12:03:09 +0900

    feat: 단일 타임캡슐 조회 추가

--- src/main/java/com/dgsw/timecapsule/domain/controller/TimeCapsuleController.java
+++ src/main/java/com/dgsw/timecapsule/domain/controller/TimeCapsuleController.java
@@ -1,11 +1,24 @@
 package com.dgsw.timecapsule.domain.controller;
 
+import com.dgsw.timecapsule.domain.dto.GetTimeCapsuleResponse;
+import com.dgsw.timecapsule.domain.entity.TimeCapsule;
+import com.dgsw.timecapsule.domain.service.GetTimeCapsuleService;
+import com.dgsw.timecapsule.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/timecapsule")
 @RequiredArgsConstructor
 public class TimeCapsuleController {
+    private final GetTimeCapsuleService getTimeCapsuleService;
+
+    @GetMapping("/{id}")
+    public ApiResponse<GetTimeCapsuleResponse> findById(@RequestParam Long id) {
+        TimeCapsule timeCapsule = getTimeCapsuleService.execute(id);
+        return ApiResponse.ok(GetTimeCapsuleResponse.to(timeCapsule));
+    }
 }
---
+++ src/main/java/com/dgsw/timecapsule/domain/service/GetTimeCapsuleService.java
@@ -0,0 +1,29 @@
+package com.dgsw.timecapsule.domain.service;
+
+import com.dgsw.timecapsule.domain.entity.TimeCapsule;
+import com.dgsw.timecapsule.domain.repository.TimeCapsuleRepository;
+import com.dgsw.timecapsule.global.exception.CustomException;
+import com.dgsw.timecapsule.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class GetTimeCapsuleService {
+    private final TimeCapsuleRepository timeCapsuleRepository;
+
+    public TimeCapsule execute(Long id) {
+        TimeCapsule timeCapsule = timeCapsuleRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+
+        LocalDateTime now = LocalDateTime.now();
+
+        if (timeCapsule.getOpenAt().isAfter(now)) {
+            throw new CustomException(ErrorCode.FORBIDDEN);
+        }
+
+        return timeCapsule;
+    }
+}
